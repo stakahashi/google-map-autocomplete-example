37d366c48c6a5f3c473c779defc949f171836bd7
Shunsuke Takahashi <stakahashi@elit.jp>
2018-03-30 14:41:45 +0900

    add element-ui

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const view = `
 <div>
-    <input v-model="input">
-
-    <ul>
-        <li v-for="row in list.predictions" @click="selected(row)">{{ row.description }}</li>
-    </ul>
-
-    <pre>{{ select }}</pre>
+    <el-input v-model="input"></el-input>
+    
+    <el-button style="margin-top: 1rem"　type="primary" v-for="row in list.predictions" :key="row.id" @click="selected(row)">{{ row.description }}</el-button>
+    
+    <el-collapse-transition>
+    <el-card style="margin-top: 1rem" v-show="Object.keys(select).length">
+        <div style="display: flex; align-items: center;" slot="header">
+            <span style="margin-right: auto">{{ select.description }}</span> <el-button @click="clear">Clear</el-button>
+        </div>
+        <div>{{ JSON.stringify(select, null, "\t") }}</div>>
+    </el-card>
+    </el-collapse-transition>
 
 </div>
 `
@@ -23,9 +28,13 @@ const GoogleMap = {
         }
     },
     methods: {
-        selected: function(row) {
+        selected(row) {
             this.select = row
-        }
+        },
+
+        clear() {
+            this.select = {}
+        },
     },
     watch: {
         input(input) {
